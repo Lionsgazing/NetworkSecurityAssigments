8ea955d8abdbb1bd8d9d14a0652b6842266b31d9
Lionsgazing <43805956+Lionsgazing@users.noreply.github.com>
2024-04-03 01:03:44 +0200

    works now!

--- NetworkAssignment/icmp_covert.py
+++ NetworkAssignment/icmp_covert.py
@@ -1,5 +1,40 @@
 import socket
 
+def create_ICMP_message(type: int, code: int, payload: str):
+    MIN_ICMP_PAYLOAD_SIZE = 4
+    MAX_ICMP_PAYLOAD_SIZE = 576
+
+    icmp_type = int.to_bytes(type, 1, 'big') # 1 Byte
+    icmp_code = int.to_bytes(code, 1, 'big') # 1 Byte
+    icmp_checksum = int.to_bytes(0, 2, 'big') # 2 Bytes
+ 
+    # Convert payload to bytes
+    icmp_total_payload = bytes(payload, 'utf-8') # X bytes, max 576.
+
+    # Check if payload is below minimum size and extend it if needed be
+    if (len(icmp_total_payload) < MIN_ICMP_PAYLOAD_SIZE):
+        icmp_total_payload = icmp_total_payload + b'\x00' * (MIN_ICMP_PAYLOAD_SIZE - len(icmp_total_payload))
+
+    # Figure out if we need to generate more messages to fit the payload in a transmission.
+    msgs_to_generate = len(payload) // MAX_ICMP_PAYLOAD_SIZE + 1 # Integer division
+
+    # Create messages needed
+    icmp_msg_collection: list[bytes] = []
+    for i in range(msgs_to_generate):
+        # Get part of payload
+        if ((i + 1) * MAX_ICMP_PAYLOAD_SIZE < len(icmp_total_payload)):
+            icmp_part_payload = icmp_total_payload[i * MAX_ICMP_PAYLOAD_SIZE:(i + 1) * MAX_ICMP_PAYLOAD_SIZE]
+        else:
+            icmp_part_payload = icmp_total_payload[i * MAX_ICMP_PAYLOAD_SIZE:]
+
+        # Construct message
+        icmp_msg = icmp_type + icmp_code + icmp_checksum + icmp_part_payload
+
+        # Add message to collection
+        icmp_msg_collection.append(icmp_msg)
+
+    return icmp_msg_collection
+
 def main():
     host = "127.0.0.1"
     port = 50007
@@ -7,7 +42,11 @@ def main():
     sockfd = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     sockfd.bind((host, port))
 
-    sockfd.sendto(b'boi', (host, port))
+
+    icmp_msg_collection = create_ICMP_message(47, 0, "HE")
+    
+    for icmp_msg in icmp_msg_collection:
+        sockfd.sendto(icmp_msg, (host, port))
 
 
 
--- NetworkAssignment/server.py
+++ NetworkAssignment/server.py
@@ -1,8 +1,76 @@
 import socket
 
+def unpack_IPV4_message(message: bytes): 
+    ipv4_version_IHL_len = 1 # Byte
+    ipv4_TOS_len = 1 # Byte
+    ipv4_total_length_len = 2 # Bytes
+    ipv4_identification_len = 2 # Bytes
+    ipv4_flags_fragment_offset_len = 2 # Bytes
+    ipv4_TTL_len = 1 # Byte
+    ipv4_protocol_len = 1 # Byte
+    ipv4_header_checksum_len = 2 # Bytes
+    ipv4_source_addr_len = 4 # Bytes
+    ipv4_dest_addr_len = 4 # Bytes
+
+
+    # HMM perhaps a stuct.unpack instead of this?
+    len_tracker = 0
+    
+    ipv4_version_IHL = message[len_tracker:len_tracker + ipv4_version_IHL_len]
+    len_tracker += ipv4_version_IHL_len
+
+    ipv4_TOS = message[len_tracker:len_tracker + ipv4_TOS_len]
+    len_tracker += ipv4_TOS_len
+
+    ipv4_total_length = message[len_tracker:len_tracker + ipv4_total_length_len]
+    len_tracker += ipv4_total_length_len
+
+    ipv4_identification = message[len_tracker:len_tracker + ipv4_identification_len]
+    len_tracker += ipv4_identification_len
+
+    ipv4_flags_fragment_offset = message[len_tracker:len_tracker + ipv4_flags_fragment_offset_len]
+    len_tracker += ipv4_flags_fragment_offset_len
+
+    ipv4_TTL = message[len_tracker:len_tracker + ipv4_TTL_len]
+    len_tracker += ipv4_TTL_len
+
+    ipv4_protocol = message[len_tracker:len_tracker + ipv4_protocol_len]
+    len_tracker += ipv4_protocol_len
+
+    ipv4_header_checksum = message[len_tracker:len_tracker + ipv4_header_checksum_len]
+    len_tracker += ipv4_header_checksum_len
+
+    ipv4_source_addr = message[len_tracker:len_tracker + ipv4_source_addr_len]
+    len_tracker += ipv4_source_addr_len
+
+    ipv4_dest_addr = message[len_tracker:len_tracker + ipv4_dest_addr_len]
+    len_tracker += ipv4_dest_addr_len
+
+    return message[len_tracker:]
+
+
+
+
+
+def unpack_ICMP_message(message: bytes):
+    icmp_type_len = 1 # Byte
+    icmp_code_len = 1 # Byte
+    icmp_checksum_len = 2 # Bytes
+
+    # Do unpacking
+    icmp_type = message[:icmp_type_len]
+    icmp_code = message[icmp_type_len:icmp_type_len + icmp_code_len]
+    icmp_checksum = message[icmp_type_len + icmp_code_len:icmp_type_len + icmp_code_len + icmp_checksum_len]
+
+    icmp_payload = message[icmp_type_len + icmp_code_len + icmp_checksum_len:]
+
+    return icmp_type, icmp_code, icmp_checksum, icmp_payload
+
+    
+
 def main():
 
-    host = "127.0.0.1"
+    host = ""
     port = 50007
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
@@ -11,8 +79,11 @@ def main():
     isRunning = True
 
     while(isRunning):
-        msg, addr = sock.recvfrom(1)
-        print(f'{addr} : {msg.decode(encoding="utf-8")}')
+        msg, addr = sock.recvfrom(100)
+
+        icmp_type, icmp_code, icmp_checksum, icmp_payload = unpack_ICMP_message(unpack_IPV4_message(msg))
+
+        print(f'{addr} : {icmp_payload}')
 
 if (__name__ == "__main__"):
     main()
